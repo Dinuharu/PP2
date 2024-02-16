4883db223ab647d8f86560d32ff276b9ed2aad5b
Dinuharu <d_nurmakov@kbtu.kz>
2024-02-16 15:18:12 +0600

    functions

---
+++ TSIS3/Functions Pt1/f_to_c.py
@@ -0,0 +1,5 @@
+def f_to_c(f):
+    return (f-32)*(5/9)
+
+f = int(input())
+print(f_to_c(f))
---
+++ TSIS3/Functions Pt1/grams_to_ounces.py
@@ -0,0 +1,5 @@
+def gramms_to_ounces(gramms):
+    return gramms/28.3495231
+
+gramms = int(input())
+print(gramms_to_ounces(gramms))
